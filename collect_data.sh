3eaef1f37c842c4df57ef480dc0ad743580e74c9
Yang Gao <gy20073@gmail.com>
2018-06-19 20:11:19 -0700

    add data collection bash

---
+++ collect_data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# you could switch the carla config through the command line, thus it's easy to loop over all carla config files.
+
+# some pending flags
+# --carla-config
+# --show_screen
+# --noise
+
+python chauffeur.py \
+    drive \
+    --gpu 0 \
+    --log \
+    --debug \
+    --path "./data" \
+    --driver-config "./drive_interfaces/configuration/9cam_agent_carla_acquire_rc_yang.py"

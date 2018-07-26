3e006a8a9b530dd5272e8d544bd43a8ba7f0203f
Yang Gao <gy20073@gmail.com>
2018-07-25 18:25:05 -0700

    polish chauffeur

--- chauffeur.py
+++ chauffeur.py
@@ -11,43 +11,40 @@ sys.path.append('structures')
 from common_util import parse_drive_arguments
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Chauffeur')
-    # General
+
     parser.add_argument('mode', metavar='mode', default='train', type=str, help='what kind of mode you are running')
-
-    parser.add_argument('-g', '--gpu', type=str, default="0", help='GPU NUMBER')
-    parser.add_argument('-lg', '--log', help="activate the log file", action="store_true")
-    parser.add_argument('-db', '--debug', help="put the log file to screen", action="store_true")
     parser.add_argument('-e', '--experiment-name',
-                        help="The experiment name (NAME.py file should be in configuration folder, and the results will be saved to models/NAME)",
+                        help='''The experiment name (NAME.py file should be in configuration folder,
+                                and the results will be saved to models/NAME)''',
                         default="")
-    parser.add_argument('-pt', '--path', type=str, default="../Desktop/", help='Path to Store or read outputs')
 
-    # Train
-
+    # resource related
+    parser.add_argument('-g', '--gpu', type=str, default="0", help='GPU NUMBER')
     parser.add_argument('-m', '--memory', default=0.9, help='The amount of memory this process is going to use')
-
-    # Drive
-
-    parser.add_argument('-dc', '--driver-config', type=str, help="The configuration of the driving file")
-    parser.add_argument('-cc', '--carla-config', help="Carla config file used for driving")
+    parser.add_argument('-pt', '--path', type=str, default="../Desktop/", help='Path to Store or read outputs')
     parser.add_argument('-l', '--host', type=str, help='The IP where DeepGTAV is running')
     parser.add_argument('-p', '--port', default=2000, help='The port where Any server to be connected is running')
 
-    parser.add_argument('-nm', '--name', type=str, default="Unknown", help='Name of the person who is going to drive')
-    parser.add_argument('-sc', '--show_screen', default=True, action="store_true",
-                        help='If we are showing the screen of the player')
-    parser.add_argument('-res', '--resolution', help='If we are showing the screen of the player')
-    parser.add_argument('-n', '--noise', help='Set the types of noise:  Spike or None')
+    # config files
+    parser.add_argument('-dc', '--driver-config', type=str, help="The configuration of the driving file")
+    parser.add_argument('-cc', '--carla-config', help="Carla config file used for driving")
+
+    # drive related
     # TODO: breaking change, from driver to type_of_driver
     parser.add_argument('--type_of_driver', help='Select who is driving, a human or a machine')
+    parser.add_argument('-n', '--noise', help='Set the types of noise:  Spike or None')
+    parser.add_argument('-imc', '--image_cut', help='Set the positions where the image is cut')
+    parser.add_argument('-nm', '--name', type=str, default="Unknown", help='Name of the person who is going to drive')
     parser.add_argument('-in', '--interface', help='The environment being used as interface')
-    parser.add_argument('-cy', '--city', type=str, help='select the graph from the city being used')
+
+    # visualization related
     parser.add_argument('-nc', '--number_screens', help='Set The number of screens that are being ploted')
     parser.add_argument('-sf', '--scale_factor', help='Set the scale of the ploted screens')
-    parser.add_argument('-up', '--use_planner', help='Check if the planner is going to be used')
-    parser.add_argument('-imc', '--image_cut', help='Set the positions where the image is cut')
+    parser.add_argument('-res', '--resolution', help='If we are showing the screen of the player')
+    # logging related
+    parser.add_argument('-lg', '--log', help="activate the log file", action="store_true")
+    parser.add_argument('-db', '--debug', help="put the log file to screen", action="store_true")
 
     args = parser.parse_args()
     know_args = parser.parse_known_args()
@@ -65,13 +62,10 @@ if __name__ == '__main__':
             root.addHandler(ch)
 
     try:
-
         if args.mode == 'drive':
             from drive import drive
-
             driver_conf_module = __import__(args.driver_config)
             driver_conf = driver_conf_module.configDrive()
-
             driver_conf = parse_drive_arguments(args, driver_conf,
                             attributes=['carla_config', 'host', 'path', 'noise',
                                       'type_of_driver', 'interface', 'number_screens', 'scale_factor'])
@@ -95,8 +89,8 @@ if __name__ == '__main__':
             from test_train import test_train
 
             test_train(args.gpu)
-        else:  # mode == evaluate
-            evaluate.evaluate(args.gpu)
+        else:
+            raise ValueError()
 
     except KeyboardInterrupt:
         os._exit(1)

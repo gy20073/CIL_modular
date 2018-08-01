7ad168c244601978e4fed8dc42208bbbd50ba899
Yang Gao <gy20073@gmail.com>
2018-07-31 17:50:07 -0700

    polish dataset manager

--- input/dataset_manager.py
+++ input/dataset_manager.py
@@ -1,82 +1,53 @@
-# import caffe
-
-import sys
-
-import h5py
+import sys, h5py, threading
+import tensorflow as tf
 
 sys.path.append('spliter')
-
 from spliter import Spliter
-
-import tensorflow as tf
-import threading
 from dataset import *
-
 
 # TODO: Divide also by acceleration and Steering
 
-
 class DatasetManager(object):
     def __init__(self, config):
-        print((config.train_db_path))
-
-        """ Read all hdf5_files """
-        # train_db_path is all data*.h5 files
-        # config.sensor_names are: ['rgb']
-        # config.dataset_names are: ['targets']
-        self._images_train, self._datasets_train = self.read_all_files(config.train_db_path, config.sensor_names,
+        self._images_train, self._datasets_train = self.read_all_files(config.train_db_path,
+                                                                       config.sensor_names,
                                                                        config.dataset_names)
-
-        self._images_val, self._datasets_val = self.read_all_files(config.val_db_path, config.sensor_names,
+        self._images_val, self._datasets_val = self.read_all_files(config.val_db_path,
+                                                                   config.sensor_names,
                                                                    config.dataset_names)
-
-        # print len(self._images_train)
-        # print self._images_train[0].shape
-        # print self._images_train
 
         spliter = Spliter(1, 1, config.steering_bins_perc)
 
-        # print self._datasets_train[0][config.variable_names.index("Steer")][:]
         #self.labels_per_division = [[0, 2, 5], [3], [4]]
         divided_keys_train = spliter.divide_keys_by_labels(
-            self._datasets_train[0][config.variable_names.index("Control")][:], config.labels_per_division)
-
+                self._datasets_train[0][config.variable_names.index("Control")][:],
+                config.labels_per_division)
         # The structure is: self._splited_keys_train[i_labels_per_division][i__steering_bins_perc][a list of keys]
         # In theory should be sharded instance ids by those two criterions
         self._splited_keys_train = spliter.split_by_output(
-            self._datasets_train[0][config.variable_names.index("Steer")][:], divided_keys_train)
-        # np.set_printoptions(threshold=np.nan)
-        # print self._datasets_train[0][config.variable_names.index("Steer")][:]
-        # print np.sort(self._datasets_train[0][config.variable_names.index("Control")][:])
+                self._datasets_train[0][config.variable_names.index("Steer")][:],
+                divided_keys_train)
 
         divided_keys_val = spliter.divide_keys_by_labels(
-            self._datasets_val[0][config.variable_names.index("Control")][:],
-            config.labels_per_division)  # THE NOISE IS NOW NONE, TEST THIS
+                self._datasets_val[0][config.variable_names.index("Control")][:],
+                config.labels_per_division)  # THE NOISE IS NOW NONE, TEST THIS
+        self._splited_keys_val = spliter.split_by_output(
+                self._datasets_val[0][config.variable_names.index("Steer")][:],
+                divided_keys_val)
 
-        self._splited_keys_val = spliter.split_by_output(self._datasets_val[0][config.variable_names.index("Steer")][:],
-                                                         divided_keys_val)
+        print("max id train", max(max(max(self._splited_keys_train))))
+        print("min id train", min(min(min(self._splited_keys_train))))
+        print("max id val", max(max(max(self._splited_keys_val))))
 
-        # self._splited_keys_train = self.divide_keys_by_labels(control_train,self._splited_keys_train,config.number_of_divisions,config.labels_per_division)
-
-
-
-
-
-        print(max(max(max(self._splited_keys_train))))
-        print('Min ID Train')
-        print(min(min(min(self._splited_keys_train))))
-
-        print('Max Id Val')
-        print(max(max(max(self._splited_keys_val))))
-        print(
-        len(self._splited_keys_train[0][0]), len(self._splited_keys_train[0][1]), len(self._splited_keys_train[0][2]))
-
-        self.train = Dataset(self._splited_keys_train, self._images_train, self._datasets_train, config, config.augment)
-        self.validation = Dataset(self._splited_keys_val, self._images_val, self._datasets_val, config,
-                                  [None] * len(config.sensor_names))
+        self.train = Dataset(self._splited_keys_train,
+                             self._images_train,
+                             self._datasets_train, config, config.augment)
+        self.validation = Dataset(self._splited_keys_val,
+                                  self._images_val,
+                                  self._datasets_val, config, [None] * len(config.sensor_names))
 
     def start_training_queueing(self, sess):
-
+        # TODO: those extra threading for enqueue operation might be unnecessary
         enqueue_thread = threading.Thread(target=self.train.enqueue, args=[sess])
         enqueue_thread.isDaemon()
         enqueue_thread.start()
@@ -85,7 +56,6 @@ class DatasetManager(object):
         self._threads_train = tf.train.start_queue_runners(coord=coord, sess=sess)
 
     def start_validation_queueing(self, sess):
-
         enqueue_thread = threading.Thread(target=self.validation.enqueue, args=[sess])
         enqueue_thread.isDaemon()
         enqueue_thread.start()
@@ -93,49 +63,25 @@ class DatasetManager(object):
         coord = tf.train.Coordinator()
         self._threads_val = tf.train.start_queue_runners(coord=coord, sess=sess)
 
-    def stop_training_queueing(self, sess):
-        sess.run(self.train.queue.close(cancel_pending_enqueues=True))
-        coord.request_stop()
-        coord.join(self._threads_train)
-
-    def stop_training_queueing(self, sess):
-        sess.run(self.train.queue.close(cancel_pending_enqueues=True))
-        coord.request_stop()
-        coord.join(self._threads_train)
-
-    def read_all_files(self, file_names, image_dataset_names, dataset_names):
-
-        datasets_cat = [list([]) for _ in range(len(dataset_names))]
-
-        images_data_cat = [list([]) for _ in range(len(image_dataset_names))]
+    def read_all_files(self, file_names, sensor_names, target_names):
+        sensor_cat = [list([]) for _ in range(len(sensor_names))]
+        targets_cat = [list([]) for _ in range(len(target_names))]
 
         lastidx = 0
-        count = 0
-        # print file_names
         for cword in file_names:
             try:
-                # print cword
-                # print count
                 dset = h5py.File(cword, "r")
-
-                for i in range(len(image_dataset_names)):
-                    # print image_dataset_names[i]
-                    x = dset[image_dataset_names[i]]
-                    # print x
+                for i in range(len(sensor_names)):
+                    x = dset[sensor_names[i]]
                     old_shape = x.shape[0]
-                    # print old_shape
+                    sensor_cat[i].append((lastidx, lastidx + x.shape[0], x))
 
-                    images_data_cat[i].append((lastidx, lastidx + x.shape[0], x))
-
-                for i in range(len(dataset_names)):
-                    dset_to_append = dset[dataset_names[i]]
-
-                    datasets_cat[i].append(dset_to_append[:])
+                for i in range(len(target_names)):
+                    dset_to_append = dset[target_names[i]]
+                    targets_cat[i].append(dset_to_append[:])
 
                 lastidx += old_shape
                 dset.flush()
-                count += 1
-                #dset.close()
 
             except IOError:
                 import traceback
@@ -146,11 +92,11 @@ class DatasetManager(object):
                                           limit=2, file=sys.stdout)
                 print("failed to open", cword)
 
-        for i in range(len(dataset_names)):
-            datasets_cat[i] = np.concatenate(datasets_cat[i], axis=0)
-            datasets_cat[i] = datasets_cat[i].transpose((1, 0))
+        for i in range(len(target_names)):
+            targets_cat[i] = np.concatenate(targets_cat[i], axis=0)
+            targets_cat[i] = targets_cat[i].transpose((1, 0))
 
-        # images_data_cat is a list for each of the variables, each of them is a list of (start_index, end_index, x) tuples
-        # datasets is a list for each of the variables, variable across batch are concatenated together and transposed to
+        # sensor_cat is a list for each of the variables, each of them is a list of (start_index, end_index, x) tuples
+        # targets_cat is a list for each of the variables, variable across batch are concatenated together and transposed to
         #       have size dim*batch
-        return images_data_cat, datasets_cat
+        return sensor_cat, targets_cat

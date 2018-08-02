55c1a53a9ce06f61120f36cc7bacb2ce678462f3
Yang Gao <gy20073@gmail.com>
2018-08-02 12:06:56 -0700

    purged a lot of deprecated flags

--- configuration/mm45_v4_from_alldata_noseg.py
+++ configuration/mm45_v4_from_alldata_noseg.py
@@ -3,10 +3,6 @@ from imgaug import augmenters as iaa
 
 class configMain:
     def __init__(self):
-        # this is used for data balancing. Labels are balanced first, and then for each label group
-        # the [-1,1] interval is split into so many equal intervals
-        # when we need to sample a mini-batch, we sample bins and then sample inside the bins
-
         self.steering_bins_perc = [0.05, 0.05, 0.1, 0.3, 0.3, 0.1, 0.05, 0.05]
         self.number_steering_bins = len(self.steering_bins_perc)
         self.batch_size = self.number_steering_bins * 15
@@ -22,14 +18,12 @@ class configMain:
                                'C_Gen', 'C_Ped', 'C_Car', 'Road_I', 'Side_I', 'Acc_x', 'Acc_y', 'Acc_z',
                                'Plat_Ts', 'Game_Ts', 'Ori_X', 'Ori_Y', 'Ori_Z', 'Control', 'Camera', 'Angle',
                                'wp1_x', 'wp1_y', 'wp2_x', 'wp2_y', 'wp1_angle', 'wp1_mag', 'wp2_angle', 'wp2_mag']
-        # _N is noise
 
         # TODO: change corresponding sensor name calls
         self.sensor_names = ['CameraMiddle']
         self.sensors_size = [self.image_size]
         self.sensors_normalize = [True]
 
-        # CHANGE THIS FOR A DICTIONARY FOR GOD SAKE
         self.targets_names = ['Steer', 'Gas', 'Brake', 'Speed']
         self.targets_sizes = [1, 1, 1, 1]
 
@@ -45,28 +39,19 @@ class configMain:
 
         # a list of keep_prob corresponding to the list of layers:
         # 8 conv layers, 2 img FC layer, 2 speed FC layers, 1 joint FC layer, 5 branches X 2 FC layers each
-        # This is error prone..... GOES TO THE NETWORK FILE DIRECTLY
         self.dropout = [0.8] * 8 + [0.5] * 2 + [0.5] * 2 + [0.5] * 1 + [0.5, 1.] * len(self.branch_config)
-
-        self.restore = True  # This is if you want to restore a saved model
 
         self.models_path = os.path.join('models', os.path.basename(__file__).split('.')[0])
         self.train_path_write = os.path.join(self.models_path, 'train')
         self.val_path_write = os.path.join(self.models_path, 'val')
-        self.test_path_write = os.path.join(self.models_path, 'test')
 
         self.number_iterations = 501000  # 500k
-
         self.perform_simulation_test = False
         self.output_is_on = True
 
         # disable the segmentation model
         self.segmentation_model = None
         #self.segmentation_model_name = "ErfNet_Small"
-
-        # deprecated flags
-        # self.pre_train_experiment = None
-        # self.extra_augment_factor = 6.0
 
 
 class configInput(configMain):
@@ -77,7 +62,6 @@ class configInput(configMain):
         oc = lambda aug: iaa.Sometimes(0.1, aug)
         rl = lambda aug: iaa.Sometimes(0.05, aug)
 
-        # Yang: actually there is only one sensor modalities
         self.augment = [iaa.Sequential([
             rl(iaa.GaussianBlur((0, 1.3))),  # blur images with a sigma between 0 and 1.5
             rl(iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05), per_channel=0.5)),  # add gaussian noise to images
@@ -94,7 +78,6 @@ class configInput(configMain):
         self.val_db_path = glob.glob("/data/yang/code/aws/scratch/carla_collect/1/*WeatherId=13/data_*.h5")
         self.train_db_path = list(set(all_files) - set(self.val_db_path))
 
-        # Speed Divide Factor
         # TODO: change the speed compatibility
         # TODO: For now is hardcooded, but eventually we should be able to calculate this from data at the loading time.
         self.speed_factor = 40.0  # In KM/H
@@ -103,15 +86,11 @@ class configInput(configMain):
         # in every mini-batch there will be equal number of samples with labels from each group
         # e.g. for [[0,1],[2]] there will be 50% samples with labels 0 and 1, and 50% samples with label 2
         self.labels_per_division = [[0, 2, 5], [3], [4]]
-
         self.dataset_names = ['targets']
-
         self.queue_capacity = 10 # now measured in how many batches
 
         # TODO: resize the images
         # TODO: use the new mode for TrainManager
-        # deprecated flags
-        # self.dataset_name = 'Carla'
 
     # TODO NOT IMPLEMENTED Felipe: True/False switches to turn data balancing on or off
 
@@ -122,7 +101,7 @@ class configTrain(configMain):
 
         self.loss_function = 'mse_branched'  # Chose between: mse_branched, mse_branched_ladd
         self.control_mode = 'single_branch_wp' # TODO: study the loss_function and control_mode
-        self.learning_rate = 0.0002  # First
+        self.learning_rate = 0.0002
         # use the default segmentation network
         #self.seg_network_erfnet_one_hot = True  # comment this line out to use the standard network
 
@@ -134,16 +113,6 @@ class configTrain(configMain):
         self.variable_weight = {'Steer': 0.1, 'Gas': 0.2, 'Brake': 0.1, 'Speed': 1.0}
         self.network_name = 'chauffeurNet_deeper'
         self.is_training = True
-
-        # deprecated flags
-        # self.train_segmentation = True # TODO: why train segmentation == True? will it be not transferrable?
-        # self.finetune_segmentation = True
-        # self.number_of_labels = 2 # number of labels in the final segmentation
-        # self.restore_seg_test = False # not used anywhere
-        # self.lambda_l2 = 1e-3  # Not used anywhere
-        # self.lambda_tolerance = 5 # Not used anywhere
-        # self.selected_gpu = "0" # Not used anywhere
-        # self.state_output_size = (0) # Not used anywhere
 
 
 class configOutput(configMain):
@@ -158,14 +127,8 @@ class configOutput(configMain):
         self.selected_branch = 0  # for the branches that have steering we also select the branch
         self.inputs_to_print = ['Speed']
 
-        # deprecated flags
-        # self.feature_save_interval = 100
-
 
 class configTest(configMain):
     def __init__(self):
         configMain.__init__(self)
         self.driver_config = "9cam_agent_carla_test_rc"  # only used, when calling the drive() function
-
-        # deprecated flags
-        # self.interface_name = 'Carla' # Not used anywhere

dca2cc3a893eef3fbbf5b63c15b30909ddfa2a4b
Yang Gao <gy20073@gmail.com>
2018-08-18 10:39:04 -0700

    train two models with NMC

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=3
 ulimit -Sn 60000
 python chauffeur.py train \
-    -e mm45_v4_from_alldata_noseg_splitv0_matthias_similar_wp \
+    -e mm45_v4_noseg_splitv0_NMC \
     -m 0.1

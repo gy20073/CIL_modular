39a82c07e89023f5d811627f751cedee8b76ff25
Yang Gao <gy20073@gmail.com>
2018-08-19 18:32:32 -0700

    add two configs

--- train.sh
+++ train.sh
@@ -3,5 +3,6 @@
 export CUDA_VISIBLE_DEVICES=2
 ulimit -Sn 60000
 python chauffeur.py train \
-    -e mm45_v4_matthias_nowp_shallow \
+    -e mm45_v4_from_alldata_noseg_splitv0_matthias_similar_wp_noseg_lessdrop \
     -m 0.1
+

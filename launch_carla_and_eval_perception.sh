ce4764ffc61ae8dfd4569030dc3982ef67491f5c
Yang Gao <gy20073@gmail.com>
2018-08-25 17:59:54 -0700

    super large timeout

--- launch_carla_and_eval_perception.sh
+++ launch_carla_and_eval_perception.sh
@@ -3,10 +3,10 @@
 # resource related
 gpu_carla="0"
 gpu_agent="1"
-port="2012"
+port="2000"
 # test related
 city_name="Town02" # first test the train town
-exp_id="mm45_v5_perception_allpercep_wd5e_6"
+exp_id="mm45_v5_perception_allpercep_nowd"
 
 # launch carla
 export CUDA_VISIBLE_DEVICES=$gpu_carla

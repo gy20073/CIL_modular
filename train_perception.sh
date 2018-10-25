e4008ce1317fe7589f21c41e5706b70c70a32a7d
Yang Gao <gy20073@gmail.com>
2018-10-25 19:34:00 +0800

    train the abn seg models

--- train_perception.sh
+++ train_perception.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_base_newseg_noiser_TL_lane_structure02_goodsteer_waypoint_zoom_stdnorm_v5_2point \
+    -e mm45_v4_base_newseg_noiser_TL_lane_structure02_goodsteer_waypoint_zoom_stdnorm_v5_abn_nozoom \
     -m 0.05
 

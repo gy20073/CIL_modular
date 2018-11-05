3d9388ad873706403499fe6eace68cb589953e6e
Yang Gao <gy20073@gmail.com>
2018-11-05 09:05:02 -0800

    add some regression control prediction models

--- train_perception.sh
+++ train_perception.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_base_newseg_noiser_TL_lane_structure02_goodsteer_waypoint_zoom_stdnorm_v5_3cam_2town_zoom3 \
+    -e mm45_v4_wp2town3cam_rawcontrol_1cam \
     -m 0.05

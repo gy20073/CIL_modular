d8df678870dd585e3c66149fd02c5ec4f7ae2672
Yang Gao <gy20073@gmail.com>
2018-11-15 07:45:14 -0800

    2p3town training

--- train_perception.sh
+++ train_perception.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_wp2town3cam_parallel_control_2p2town_real \
+    -e mm45_v4_wp2town3cam_parallel_control_2p3town \
     -m 0.05

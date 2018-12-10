235a516508c63e2ef4e335d9e7879ff25641ca9f
Yang Gao <gy20073@gmail.com>
2018-12-10 14:33:50 -0800

    confirm that the training loss is too high, and try without the sigma constraint

--- train_perception.sh
+++ train_perception.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_wp2town3cam_parallel_control_2p3town_map_sensor_dropout_cs2fn_steerfocus_LargeMapNoise \
+    -e mm45_v4_wp2town3cam_parallel_control_2p3town_map_sensor_dropout_cs2fn_steerfocus_LargeMapNoise_nosigma \
     -m 0.05
 
 

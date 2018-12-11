dacf8495911e3d85b6e3d1c358d65f9c6a660d2d
Yang Gao <gy20073@gmail.com>
2018-12-11 15:14:34 -0800

    make the modification to allow data collection in RFS

--- drive_multi_config.py
+++ drive_multi_config.py
@@ -1,13 +1,13 @@
 import sys, os, time, threading
 
-TownName = "Town01"
+TownName = "RFS_MAP"
 start_port=2100
 available_gpus = [0]
 num_processes = 4
 use_docker = False
 # 9cam_agent_carla_acquire_rc_batch_090, change its contents
 
-if TownName == "Town03" or TownName == "Town04":
+if TownName == "Town03" or TownName == "Town04" or TownName == "RFS_MAP":
     CARLA_PATH = "/scratch/yang/aws_data/carla_auto2/CarlaUE4.sh"
     os.environ['CARLA_VERSION'] = '0.9.auto2'
     docker_path = "gy20073/carla_auto2:latest"

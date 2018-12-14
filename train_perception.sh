a8013541944c6af4604412da7513a736f1fd633e
Yang Gao <gy20073@gmail.com>
2018-12-13 23:22:02 -0800

    two more configs

--- train_perception.sh
+++ train_perception.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_wp2town3cam_parallel_control_2p3town_map_sensor_dropout_GMM \
+    -e mm45_v4_wp2town3cam_parallel_control_2p3town_map_sensor_dropout_rfssim_moremap \
     -m 0.05
 
 

c9d1213de5f0b4d76699e1c88858238ffab75718
Yang Gao <gy20073@gmail.com>
2019-01-29 14:35:54 -0800

    adding the v5 data without lane color

--- train_perception.sh
+++ train_perception.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_PcSensordropLessmap_rfsv5_extra_structure_noise_lanecolor \
+    -e mm45_v4_PcSensordropLessmap_rfsv5_extra_structure_noise \
     -m 0.05

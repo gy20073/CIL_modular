01a344719ef7882def25f9a66e6290be045ff523
Yang Gao <gy20073@gmail.com>
2019-02-18 09:18:28 -0800

    the accurate map version of the onroad loss

--- train_perception.sh
+++ train_perception.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES=1
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_PcSensordropLessmap_rfsv45_extra_structure_noise_nocolor_onroad \
+    -e mm45_v4_PcSensordropLessmap_rfsv45_extra_structure_noise_nocolor_onroad_accurate_map \
     -m 0.05

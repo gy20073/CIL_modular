4b6352d73992102a2bea40d73cb5f7928f470e34
Yang Gao <gy20073@gmail.com>
2019-03-01 11:40:11 -0800

    font diff

--- drive_interfaces/carla/comercial_cars/carla_machine.py
+++ drive_interfaces/carla/comercial_cars/carla_machine.py
@@ -226,7 +226,7 @@ class CarlaMachine(Driver):
                  5.0: "straight",
                  0.0: "goal"}
         font_sz = int(5.0 / 88 * sensor.shape[0]) + 1
-        font_sz = min(font_sz, 35)
+        font_sz = min(font_sz, 25)
         viz = self.write_text_on_image(sensor, txtdt[direction]+extra, font_sz)
         return viz
 

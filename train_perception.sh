6deaa311c971b13c7309c0b5c53484e640c6e4e0
Yang Gao <gy20073@gmail.com>
2019-06-10 15:33:14 -0700

    add two more modules

--- train_perception.sh
+++ train_perception.sh
@@ -3,6 +3,6 @@
 export CUDA_VISIBLE_DEVICES=3
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_SqnoiseShoulder_exptownv2_notown0102_mergefollowstraight \
+    -e mm45_v4_SqnoiseShoulder_exptownv2v3_notown0102_mergefollowstraight \
     -m 0.05
 

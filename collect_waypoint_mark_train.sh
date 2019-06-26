7c7bc7f3af4a9734f32372889ee0d533627b8221
Yang Gao <gy20073@gmail.com>
2019-06-26 11:23:33 +0800

    revert back to the v19 collection and remove the CarlaUE4 kill command

--- collect_waypoint_mark_train.sh
+++ collect_waypoint_mark_train.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-inputds="exptown_v21_shoulder_overexplore"
-expid="mm45_v4_SqnoiseShoulder_exptownv9v21_notown0102"
-policy_gpu=5
+inputds="exptown_v19_weather"
+expid="mm45_v4_SqnoiseShoulder_exptownv19_notown0102"
+policy_gpu=2
 
 townid="11"
 # config ends
@@ -12,7 +12,7 @@ outputds=$inputds"_way"
 # TODO modify drive_multi_config, 9cam_agent_carla_acquire_rc_batch_095 and then
 python drive_multi_config.py
 
-pkill -f -9 CarlaUE4
+#pkill -f -9 CarlaUE4
 
 python utils/compute_waypoints.py -input=$inputds -output=$outputds
 python utils/mark_h5_townid.py --dataset=$outputds --townid=$townid

67677c48d1356ad28a2804128affba5314a9f1ce
Yang Gao <gy20073@gmail.com>
2019-06-28 12:03:20 +0800

    change the exploration to the early front of the shoulder

--- drive_multi_config.py
+++ drive_multi_config.py
@@ -3,7 +3,7 @@ import sys, os, time, threading
 TownName = "Exp_Town"
 start_port=2400
 available_gpus = [0]
-num_processes = 1
+num_processes = 2
 use_docker = False
 driver_config = "9cam_agent_carla_acquire_rc_batch_095"
 #driver_config = "9cam_agent_carla_acquire_rc_batch_090"

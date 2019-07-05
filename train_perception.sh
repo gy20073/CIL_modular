e97b2c62cda339593217ef581a7bc3e0b0cc4707
Yang Gao <gy20073@gmail.com>
2019-07-05 15:22:18 +0800

    test the shoulder percentage

--- train_perception.sh
+++ train_perception.sh
@@ -3,6 +3,6 @@
 export CUDA_VISIBLE_DEVICES=7
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_SqnoiseShoulder_exptownv9v25v26_notown0102_mapv3_fixed \
+    -e mm45_v4_SqnoiseShoulder_full_nopark_div8 \
     -m 0.05
 

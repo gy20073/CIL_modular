eb2557be207e832be52b7726524bcbac01c33934
Yang Gao <gy20073@gmail.com>
2019-07-06 16:15:29 +0800

    train the no shoulder and no zebra models

--- train_perception.sh
+++ train_perception.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=5
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_SqnoiseShoulder_full_nopark_div8 \
+    -e mm45_v4_SqnoiseShoulder_noshoulder \
     -m 0.05
 

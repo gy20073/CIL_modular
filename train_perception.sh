b86b410150dee39c710b8702418a43f3db8793f8
Yang Gao <gy20073@gmail.com>
2019-09-26 00:37:33 -0700

    add a fixTL config

--- train_perception.sh
+++ train_perception.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=3
 ulimit -Sn 600000
 python chauffeur.py train \
-    -e mm45_v4_SqnoiseShoulder_v19v25v26_expmaplessshoulder_onshoulder \
+    -e mm45_v4_SqnoiseShoulder_rfsv6_withTL_fixTL \
     -m 0.05
 
 
